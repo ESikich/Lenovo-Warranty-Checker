2689e4e22cd8fd1224d322644d8fb30804a3b1c9
ESikich <erik.sikich@gmail.com>
2023-04-25 16:04:11 -0500

    Update ad.py
    Comments and type hints

--- ad.py
+++ ad.py
@@ -3,24 +3,61 @@ from ldap3 import Server, Connection, ALL, SUBTREE
 import ldap3
 from warranty_checker import process_entry
 from tqdm.contrib.concurrent import process_map
+from typing import Dict, Any, List, Optional
 
 class AD:
-    def __init__(self, config):
+    def __init__(self, config: Dict[str, Any]):
+        """
+        Initialize the AD object.
+
+        Args:
+            config (Dict[str, Any]): The configuration for the AD object.
+        """
         self.config = config
         self.server = Server(self.config['ldap_server'], use_ssl=True, get_info=ALL)
         self.conn = None
 
     def connect(self):
-        self.conn = Connection(self.server, self.config['ad_user'], self.config['ad_password'], auto_bind=True, client_strategy=ldap3.RESTARTABLE)
+        """
+        Connect to the LDAP server.
+        """
+        self.conn = Connection(
+            self.server,
+            self.config['ad_user'],
+            self.config['ad_password'],
+            auto_bind=True,
+            client_strategy=ldap3.RESTARTABLE
+        )
 
     def close_connection(self):
+        """
+        Close the LDAP connection if it exists.
+        """
         if self.conn:
             self.conn.unbind()
 
-    def get_dev_pool(self):
-        self.conn.search(search_base=self.config['search_base'], search_filter='(objectClass=computer)', attributes=['name'], search_scope=SUBTREE)
+    def get_dev_pool(self) -> List[Optional[str]]:
+        """
+        Get a list of devices in the development pool.
 
-        results = process_map(process_entry, self.conn.entries, [self.config] * len(self.conn.entries), max_workers=mp.cpu_count(), desc='Looking for workstations')
+        Returns:
+            List[Optional[str]]: A list of device names in the development pool.
+        """
+        self.conn.search(
+            search_base=self.config['search_base'],
+            search_filter='(objectClass=computer)',
+            attributes=['name'],
+            search_scope=SUBTREE
+        )
+
+        # Process the entries in parallel
+        results = process_map(
+            process_entry,
+            self.conn.entries,
+            [self.config] * len(self.conn.entries),
+            max_workers=mp.cpu_count(),
+            desc='Looking for workstations'
+        )
 
         dev_pool = []
         for result in results:
