12bcebb33e688ee42e7169343a40c22a4a7b9a59
ESikich <erik.sikich@gmail.com>
2023-04-25 16:07:11 -0500

    Update net_util.py
    type hints and comments

--- net_util.py
+++ net_util.py
@@ -1,9 +1,20 @@
 import socket
 
 class NetUtil:
-
     @staticmethod
-    def get_ip(hostname):
+    def get_ip(hostname: str) -> str:
+        """
+        Get the IP address for a given hostname.
+
+        Args:
+            hostname: A string representing the hostname to look up.
+
+        Returns:
+            A string representing the IP address associated with the hostname.
+
+        Raises:
+            Any exceptions raised by `socket.gethostbyname`.
+        """
         try:
             ip_addr = socket.gethostbyname(hostname)
             return ip_addr
@@ -11,7 +22,21 @@ class NetUtil:
             raise
 
     @staticmethod
-    def ping(ip_addr, port=445, timeout=1):
+    def ping(ip_addr: str, port: int = 445, timeout: int = 1) -> bool:
+        """
+        Check if a given IP address and port are reachable.
+
+        Args:
+            ip_addr: A string representing the IP address to check.
+            port: An integer representing the port number to check. Defaults to 445.
+            timeout: An integer representing the timeout value in seconds. Defaults to 1.
+
+        Returns:
+            A boolean indicating whether the IP address and port are reachable.
+
+        Raises:
+            Any exceptions raised by `socket.create_connection`.
+        """
         try:
             with socket.create_connection((ip_addr, port), timeout=timeout):
                 return True
