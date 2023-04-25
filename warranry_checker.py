b3208e5294006da28f7f860112fed5feccded9bf
ESikich <erik.sikich@gmail.com>
2023-04-25 16:16:09 -0500

    Update warranry_checker.py
    uploaded wrong file

--- warranry_checker.py
+++ warranry_checker.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Optional, Tuple
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -11,18 +12,29 @@ from tqdm import tqdm
 from tqdm.contrib.concurrent import process_map
 import winrm
 
+
 class WarrantyChecker:
-    def __init__(self, config, dev_pool):
+    def __init__(self, config: Dict[str, str], dev_pool: List[Tuple[str, str]]) -> None:
         self.config = config
         self.dev_pool = dev_pool
+        self.driver: Optional[webdriver.Firefox] = None
 
-    def setup(self):
+    def setup(self) -> webdriver.Firefox:
+        """Set up the Firefox driver with headless mode enabled and return it."""
         opts = Options()
         opts.add_argument('-headless')
         driver = webdriver.Firefox(options=opts, service=FirefoxService(executable_path=self.config['driver_location']))
         return driver
 
-    def get_serial(self, device_info):
+    def get_serial(self, device_info: Tuple[str, str, str, str]) -> Optional[str]:
+        """Get the serial number of a device using Windows Management Instrumentation (WMI).
+
+        Args:
+            device_info: A tuple containing device name, IP address, WMI username, and WMI password.
+
+        Returns:
+            The serial number of the device, or None if an error occurs.
+        """
         dev_name, ip_addr, wmi_user, wmi_password = device_info
         session = winrm.Session(dev_name, auth=(wmi_user, wmi_password), transport='ntlm')
         try:
@@ -48,37 +60,64 @@ class WarrantyChecker:
         return serial_number
 
     @staticmethod
-    def get_warranty(device_info):
+    def get_warranty(device_info: Tuple[str, str, str, Dict[str, str]]) -> Tuple[str, str, str, str]:
+        """Get the warranty information of a device using its serial number.
+
+        Args:
+            device_info: A tuple containing device name, IP address, serial number, and configuration parameters.
+
+        Returns:
+            A tuple containing device name, IP address, serial number, and warranty information.
+        """
         dev_name, ip_addr, serial_number, config = device_info
         url = config['web_url']
+
+        # Set up the Firefox driver with headless mode enabled
         opts = Options()
         opts.add_argument('-headless')
         driver = webdriver.Firefox(options=opts, service=FirefoxService(executable_path=config['driver_location']))
+
+        # Navigate to the web page and enter the serial number
         driver.get(url)
         input_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, config['input_xpath'])))
         input_field.clear()
         input_field.send_keys(serial_number)
         input_field.send_keys(Keys.RETURN)
+
+        # Get the warranty information and quit the driver
         warranty_info = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, config['warranty_xpath']))).text
         driver.quit()
+
         return (dev_name, ip_addr, serial_number, warranty_info)
 
-    def proc_pool(self):
-        devs = []
-        for dev_name, ip_addr in self.dev_pool:
-            devs.append((dev_name, ip_addr, self.config['wmi_user'], self.config['wmi_password']))
 
-        # Use process_map() to get the serial numbers in parallel
-        serial_numbers = process_map(self.get_serial, devs, desc='Getting serial numbers', max_workers=4)
+def proc_pool(self) -> None:
+    """Process the device pool, get the serial numbers, and check the warranties."""
+    devs = []
+    for dev_name, ip_addr in self.dev_pool:
+        devs.append((dev_name, ip_addr, self.config['wmi_user'], self.config['wmi_password']))
 
-        pared_down_dev_pool = []
-        for device_info, serial_number in zip(devs, serial_numbers):
-            dev_name, ip_addr, _, _ = device_info
-            if not serial_number:
-                CSVHandler.err_csv(dev_name, 'Invalid Serial Number')
-                continue
+    # Use process_map() to get the serial numbers in parallel
+    serial_numbers = process_map(self.get_serial, devs, desc='Getting serial numbers', max_workers=4)
 
-            pared_down_dev_pool.append((dev_name, ip_addr, serial_number))
+    pared_down_dev_pool = []
+    for device_info, serial_number in zip(devs, serial_numbers):
+        dev_name, ip_addr, _, _ = device_info
+        if not serial_number:
+            CSVHandler.err_csv(dev_name, 'Invalid Serial Number')
+            continue
 
-        if pared_down_dev_pool:
-            self.driver = self.setup()
+        pared_down_dev_pool.append((dev_name, ip_addr, serial_number))
+
+    if pared_down_dev_pool:
+        self.driver = self.setup()
+
+    # Use process_map() to check the warranties in parallel
+    warranty_infos = process_map(self.get_warranty, [(dev_name, ip_addr, serial_number, self.config) for dev_name, ip_addr, serial_number in pared_down_dev_pool], desc='Checking warranties', max_workers=4)
+
+    # Save the warranty information to a CSV file
+    CSVHandler.write_csv(warranty_infos)
+
+    # Close the driver
+    if self.driver:
+        self.driver.quit()
